09178a2582087fd703353cc16c5a22d51da0bea7
Tonipenyallop <kimtesu521@gmail.com>
2026-06-19 17:26:15 +0900

    add normalizer

---
+++ src/webcrawler/normalize.py
@@ -0,0 +1,20 @@
+from urllib.parse import urljoin, urlsplit, urlunsplit
+import re
+
+
+def normalize_url(url: str, base: str | None = None) -> str:
+    if base:
+        url = urljoin(base, url)
+
+    tmp = urlsplit(url)
+
+    path = tmp.path or "/"
+
+    # eg) http://example.com/about
+    if len(path) > 1 and path.endswith("/"):
+        path = path.rstrip('/')
+
+    out = urlunsplit((tmp.scheme, tmp.hostname,
+                      path, tmp.query, ""))
+
+    return out
