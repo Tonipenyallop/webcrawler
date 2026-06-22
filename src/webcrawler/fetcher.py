2b8dcfb2b2e8905866956b88feea6bf22442bcff
Tonipenyallop <kimtesu521@gmail.com>
2026-06-22 16:17:17 +0900

    update fetcher w/ dataclass

--- src/webcrawler/fetcher.py
+++ src/webcrawler/fetcher.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import httpx
 
 from typing import Final
@@ -15,10 +15,10 @@ class FetchResult:
     final_url: str
 
 
+@dataclass
 class Fetcher:
-    def __init__(self):
-        self._client = httpx.AsyncClient(
-            follow_redirects=True, timeout=TIME_OUT_IN_SEC)
+    _client: httpx.AsyncClient = field(default_factory=lambda: httpx.AsyncClient(
+        follow_redirects=True, timeout=TIME_OUT_IN_SEC))
 
     async def fetch(self, url) -> FetchResult:
         res = await self._client.get(url)
